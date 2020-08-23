d038d7c074cb25f2d7fa18bcc5bd81dc0b62ded0
92321623kh <huisrt3813@pasco.o.jp>
2020-08-23 11:03:54 +0900

    Sample302

---
+++ Sample302/Sample302/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    int k = i + j;
+                    Console.Write(i + "+"+ j +"=" + k +"");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
