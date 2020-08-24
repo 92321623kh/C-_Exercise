2791652017ee051cfe121319d6e4840ae4494db9
92321623kh <huisrt3813@pasco.o.jp>
2020-08-24 17:26:58 +0900

    練習３

---
+++ renshu3/renshu3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace renshu3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a =:");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("b =:");
+            double b = double.Parse(Console.ReadLine());
+
+
+            Console.Write("c =:");
+            double c = double.Parse(Console.ReadLine());
+
+            double total = a + b + c;
+            double average = total / 3.0;
+            Console.WriteLine("a + b + c = {0}", total);
+            Console.WriteLine("abcの平均 = {0}", average);
+        }
+    }
+}
