c605e8dec5cf4254ec637b71bf30f3e6645be464
92321623kh <huisrt3813@pasco.o.jp>
2020-08-24 14:12:09 +0900

    ５日目課題３

---
+++ Day5_Exersise_3/Day5_Exersise_3/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Exersise_3
+{
+    class Car : IDrive, IMechAnimal
+    {
+        public void Drive()
+        {
+            Console.WriteLine("運転する");
+        }
+        public void Maintain()
+        {
+            Console.WriteLine("メンテナンスする");
+        }
+    }
+}
---
+++ Day5_Exersise_3/Day5_Exersise_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Exersise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void DriverACar(Car car)
+            {
+                car.Drive();
+
+            }
+            static void MaintainACar(string[] args)
+            {
+                car.Maintain();
+            }
+            static void Main(string[] args)
+            {
+                Car c = new Car();
+                DriverACar(c);
+                MaintainACar(c);
+            }
+        }
+    }
+}
