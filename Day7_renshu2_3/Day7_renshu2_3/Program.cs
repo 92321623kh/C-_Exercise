eb455d2a08cf247d6b4b7922378431e091e8fd1a
92321623kh <huisrt3813@pasco.o.jp>
2020-08-25 11:31:25 +0900

    Day7_renshu2_3

---
+++ Day7_renshu2_3/Day7_renshu2_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_renshu2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("テストの値を入力してください (0-100)");
+            int n = int.Parse(Console.ReadLine());
+            if (n >= 0 && n <= 100)
+            {
+                if (n >= 80)
+                {
+                    Console.Write("優");
+                }
+                else if (n >= 70)
+                {
+                    Console.Write("良");
+                }
+                else if (n >= 60)
+                {
+                    Console.Write("可");
+                }
+                else
+                {
+                    Console.Write("不可");
+                }
+            }
+   
+        else
+        {
+            Console.Write("不適切");
+        }
+      }
+
+    }
+}
