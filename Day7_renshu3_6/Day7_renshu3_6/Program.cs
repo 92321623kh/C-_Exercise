b144547e328cab4eb3b3825600b1ed64083715a6
92321623kh <huisrt3813@pasco.o.jp>
2020-08-25 13:32:35 +0900

    Day7_renshu3_6

---
+++ Day7_renshu3_6/Day7_renshu3_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_renshu3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("文字列を入力:");
+                string s = Console.ReadLine();
+                if (s == "")
+                {
+                    break;
+                }
+                Console.WriteLine(s);
+            }
+            
+        }
+    }
+}
