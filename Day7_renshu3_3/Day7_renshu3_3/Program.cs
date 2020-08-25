1bcaeff73cb6d19e60de453a41717219b4ab16dd
92321623kh <huisrt3813@pasco.o.jp>
2020-08-25 12:57:56 +0900

    Day7_renshu3_3

---
+++ Day7_renshu3_3/Day7_renshu3_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_renshu3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("回数を入力:");
+            int n = int.Parse(Console.ReadLine());
+            for (int i =n; i >= 0; i--)
+            {
+                Console.Write("{0}", n);
+            }
+        }
+    }
+}
