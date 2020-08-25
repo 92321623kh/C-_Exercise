09d7db03ee576cd147004081cf5de982e6bedcba
92321623kh <huisrt3813@pasco.o.jp>
2020-08-25 10:36:52 +0900

    練習４

---
+++ renshu4/renshu4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace renshu4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("幅=:");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("奥行=:");
+            double depth = double.Parse(Console.ReadLine());
+            Console.Write("高さ=:");
+            double hight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("立方体の体積　= {0}cm3", width * depth * hight);
+        }
+    }
+}
