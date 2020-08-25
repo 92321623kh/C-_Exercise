3e13b029b00fe5dea38298f0db2951e7ccd205dc
92321623kh <huisrt3813@pasco.o.jp>
2020-08-25 11:12:43 +0900

    Day7_renshu2_1

---
+++ Day7_renshu2_1/Day7_renshu2_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_renshu2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n == 4)
+            {
+                Console.WriteLine("{0}です。", n);
+            }
+        }
+    }
+}
