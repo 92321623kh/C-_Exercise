427314e690919caaadf554cf62b3272c48c0d5e8
92321623kh <huisrt3813@pasco.o.jp>
2020-08-25 11:47:19 +0900

    Day7_renshu3_2

---
+++ Day7_renshu3_2/Day7_renshu3_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_renshu3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("forループ:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("★");
+            }
+            Console.WriteLine();
+            Console.Write("whileループ:");
+            int n = 0;
+            while (n < 5)
+            {
+                Console.Write("★");
+                n++;
+            }
+            Console.WriteLine();
+            Console.Write("do～whileループ:");
+
+            n = 0;
+            do
+            {
+                Console.Write("★");
+                n++;
+            }
+            while (n < 5);
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
