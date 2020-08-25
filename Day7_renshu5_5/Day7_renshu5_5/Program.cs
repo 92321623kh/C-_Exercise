6694e17218e03bfa283f5f1a7c9f44d7631e875d
92321623kh <huisrt3813@pasco.o.jp>
2020-08-25 13:21:54 +0900

    Day7_renshu3_5

---
+++ Day7_renshu5_5/Day7_renshu5_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_renshu5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = -1, min = 101;
+            Random rnd = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int n = rnd.Next(1, 101);
+                Console.Write("{0} ", n);
+                if(n > max)
+                {
+                    max = n;
+                }
+                if(n < min)
+                {
+                    min = n;
+                }
+                Console.WriteLine();
+                Console.WriteLine("最大値:{0}", max);
+                Console.WriteLine("最小値:{0}", min);
+            }
+        }
+    }
+}
